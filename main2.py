2aa306f7e2a6bbdbb4c599214e7b15d7c65bf1fe
Kimpembie2 <biello.gabriele@iisgalvanimi.edu.it>
2024-11-08 11:03:17 +0000

    commit1

--- main2.py
+++ main2.py
@@ -8,18 +8,18 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO Animali_Domestici (id, nome, razza, eta, peso, colore) VALUES (%s, %s, %s, %s, %s, %s)"
+sql = "INSERT INTO Animali_Domestici (nome, razza, eta, peso, colore) VALUES (%s, %s, %s, %s, %s)"
 
 val = [
-  (1, 'Fido', 'Labrador', 3, 30,  'Marrone'),
-  (2, 'Micia', 'Persiano', 5, 4, 'Bianco e nero'),
-  (3, 'Rocky', 'Golden Retriever', 2, 28, 'Dorato'),
-  (4, 'Luna', 'Siamese', 4, 3, 'Grigio'),
-  (5, 'Leo', 'British Shorthair', 1, 6, 'Grigio blu'),
-  (6, 'Stella', 'Border Collie', 1, 18, 'Bianco e nero'),
-  (7, 'Max', 'Pastore tedesco', 6, 35, 'Nero e marrone'),
-  (8, 'Nina', 'Chihuahua', 2, 2, 'Marrone'),
-  (9, 'Charlie', 'Canarino', 1, 20, 'Giallo')
+  ('Fido', 'Labrador', 3, 30,  'Marrone'),
+  ('Micia', 'Persiano', 5, 4, 'Bianco e nero'),
+  ('Rocky', 'Golden Retriever', 2, 28, 'Dorato'),
+  ('Luna', 'Siamese', 4, 3, 'Grigio'),
+  ('Leo', 'British Shorthair', 1, 6, 'Grigio blu'),
+  ('Stella', 'Border Collie', 1, 18, 'Bianco e nero'),
+  ('Max', 'Pastore tedesco', 6, 35, 'Nero e marrone'),
+  ('Nina', 'Chihuahua', 2, 2, 'Marrone'),
+  ('Charlie', 'Canarino', 1, 20, 'Giallo')
 ]
 
 
